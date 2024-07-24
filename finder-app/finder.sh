508f784588eef1fdc8155bd6b24ac347ae3dd288
Kevin Flynn <2982824673@mail.nwpu.edu.cn>
2024-07-24 08:54:41 -0600

    add manual linux image and roorfs build script in assiment 3 part2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #Accepts the following runtime arguments: 
 #the first argument is a path to a directory on the filesystem, referred to below as filesdir; 
 #the second argument is a text string which will be searched within these files, referred to below as searchstr
@@ -12,7 +12,8 @@
 #where X is the number of files in the directory and all subdirectories and Y is the number of matching lines found in respective files, 
 #where a matching line refers to a line which contains searchstr (and may also contain additional content).
 
-
+set -e
+set -u
 
 filesdir=$1
 searchstr=$2
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,7 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-
+#make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- clean
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -34,11 +34,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # TODO: Add your kernel build steps her
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_PATH=${OUTDIR}/ -j$(nproc) all
 fi
 
-echo "Adding the Image in outdir"
 
+echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -48,6 +52,10 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs && cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +64,85 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+if [ ! -e ${OUTDIR}/busybox/busybox ]; then
+    make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
+fi
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+#"program interpreter"
+sysroot=$(${CROSS_COMPILE}gcc --print-sysroot)
+prog_interpreter=$(${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | tr -d "[]")
+delem=":"
+path_to_interpreter=${prog_interpreter#*$delem}
+interpreter=$(basename $path_to_interpreter | tr -d " ")
+path_prefix_interpret=$(dirname $path_to_interpreter)
+final_path=${OUTDIR}/rootfs/$path_prefix_interpret
+echo $final_path
+sudo cp $(find $sysroot -name $interpreter) $final_path
+
+#"Shared library"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | tr -d "[]" >tmp.txt
+int=0
+echo $sysroot
+
+while read -r line
+do
+    shared=$(echo ${line#*$delem} | tr -d " ")
+    for path_to_shared in $(find $sysroot -name $shared) ;  do
+        echo 
+        realtive=${path_to_shared#*$sysroot}
+        tmp_path=$(dirname $realtive)
+        final_path=${OUTDIR}/rootfs/$tmp_path
+        if [ ! -d "${final_path}" ] ; then
+            echo final_path:$final_path
+            mkdir -p $final_path
+        fi
+        echo "copy from $path_to_shared  to $final_path"
+        sudo cp $path_to_shared $final_path
+    done
+done < tmp.txt
+
+rm tmp.txt
 
 # TODO: Make device nodes
-
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 # TODO: Clean and build the writer utility
-
+cd ${FINDER_APP_DIR}/
+echo "cleaning the writer app ..."
+make CROSS_COMPILE=${CROSS_COMPILE} clean
+echo "Building the writer app ..."
+make CROSS_COMPILE=${CROSS_COMPILE} -j$(nproc)
+cp writer ${OUTDIR}/rootfs/home/
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
+ #finder.sh, conf/username.txt, conf/assignment.txt and finder-test.sh
+if [ ! -e ${OUTDIR}/rootfs/home/finder-test.sh ]; then
+    echo " copy finder.sh, conf/username.txt, conf/assignment.txt and finder-test.sh into rootfs/home ..."
+    cp finder.sh finder-test.sh ${OUTDIR}/rootfs/home
+    cp -r ../conf ${OUTDIR}/rootfs/home/
+    sed -i -e 's/..\/conf\/assignment.txt/conf\/assignment.txt/g' ${OUTDIR}/rootfs/home/finder-test.sh
+    cp autorun-qemu.sh  ${OUTDIR}/rootfs/home/
+fi
+ls ${OUTDIR}/rootfs/home
+cd ${OUTDIR}/rootfs
 # TODO: Chown the root directory
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -o --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
